f5694c9b0e285b8a3df6184614bf1ecd02972952
niharikathunga-sudo <niharikathunga@gmail.com>
2025-09-20 12:31:07 -0500

    Add files via upload

--- dictionary2.py
+++ dictionary2.py
@@ -1,5 +1,7 @@
 dictionary={}
 while True:
+    print("-"*50)
+    print("")
     print("Welcome to the dictionary app! Here are your options.. \n1.Add/Update a word..\n2.Retrieve a word's definition\n3.Delete a word\n4.View all words\n5.Exit")
     choice=input("Enter your choice..")
     if choice=="1":
@@ -16,9 +18,23 @@ while True:
         else:
             print("YOUR NONEXISTENT WORD DOESNT EXIST.. TRY AGAIN!!")
 """
+    elif choice=="3":
+        delete=input("What word do you want to delete from the dictionary?")
+        if delete in dictionary:
+            del dictionary[delete]
+        else:
+            print("Word not available in dictionary..")
+
+    elif choice=="4":
+        view=input("Do you want to see all your words?")
+        if dictionary:
+            for i in dictionary:
+                print(i,dictionary[i])
+        else:
+            print("There are no words in your dictionary to view...")
+
+    elif choice=="5":
+        print("Bye bye!! :)")
+        break
     
-
-
-
-
 
